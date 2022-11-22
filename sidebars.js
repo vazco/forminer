bf111af6d1b58b28c07ca688bd4d808866c5c780
Krzysztof Szut <krzysztof.szut@vazco.eu>
2022-11-22 13:31:04 +0100

    Added `What is Form Builder` page.

--- sidebars.js
+++ sidebars.js
@@ -24,6 +24,7 @@ const sidebars = {
       collapsed: false,
       collapsible: false,
       items: [
+        'what-is-form-builder',
         'supported-stack',
         'technical-requirements',
         'installation',
