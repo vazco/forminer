28407783001635183f01cb26cd4a6dd21114cb72
Mikolaj Kaliszewski <42475735+mkaliszewski@users.noreply.github.com>
2022-12-22 16:31:42 +0300

    FB-263 Embedding Form Builder demo in docs site (#12)

---
+++ src/pages/demo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function Demo() {
+  return (
+    <Layout title="Form Builder demo" wrapperClassName="demo-container">
+      <iframe
+        title="Form Builder demo"
+        src="https://vazco.github.io/uniforms-form-builder/"
+      />
+    </Layout>
+  );
+}
