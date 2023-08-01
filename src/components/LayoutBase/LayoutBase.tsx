286742505d04abe3aabff1b6b13e2cb95d4dd6c8
paulinabjacykowska <paulina.b.jacykowska@gmail.com>
2023-08-01 19:59:46 +0200

    add changes after review

--- src/components/LayoutBase/LayoutBase.tsx
+++ src/components/LayoutBase/LayoutBase.tsx
@@ -1,13 +1,9 @@
-import { useHistory } from '@docusaurus/router';
 import { MuiThemeProvider } from '@material-ui/core/styles';
-import React, { ReactNode, useEffect } from 'react';
+import React, { ReactNode } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyle, theme, MuiTheme } from '../../globalStyles/theme';
-import {
-  handleNavbarBackground,
-  handleNavbarScroll,
-} from '../../utils/navbarChangeHandlers';
+import { useNavbarStyles } from '../../hooks/useNavbarStyles';
 
 // import { Cookies } from '~components/molecules/Cookies';
 // import SEO from '~components/organisms/SEO';
@@ -18,19 +14,7 @@ type LayoutBaseProps = {
 };
 
 export const LayoutBase = ({ children }: LayoutBaseProps) => {
-  const history = useHistory();
-
-  useEffect(() => {
-    history.location.pathname === '/' && handleNavbarBackground();
-  }, [history]);
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleNavbarScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleNavbarScroll);
-    };
-  }, []);
+  useNavbarStyles();
 
   return (
     <MuiThemeProvider theme={MuiTheme}>
