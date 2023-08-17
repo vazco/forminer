3ece0c776cd90ed8eb6fc4da0c37b94f8e619a6d
paulinabjacykowska <paulina.b.jacykowska@gmail.com>
2023-08-17 09:16:58 +0200

    add scripts for VideoAsk

--- src/components/LayoutBase/LayoutBase.tsx
+++ src/components/LayoutBase/LayoutBase.tsx
@@ -1,5 +1,5 @@
 import { MuiThemeProvider } from '@material-ui/core/styles';
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyle, theme, MuiTheme } from '../../globalStyles/theme';
@@ -15,6 +15,33 @@ type LayoutBaseProps = {
 
 export const LayoutBase = ({ children }: LayoutBaseProps) => {
   useNavbarStyles();
+
+  useEffect(() => {
+    const videoAskScript = document.createElement('script');
+    videoAskScript.innerHTML = `
+      window.VIDEOASK_EMBED_CONFIG = {
+        "kind": "widget",
+        "url": "https://www.videoask.com/fno6cfau6",
+        "options": {
+          "widgetType": "VideoThumbnailExtraLarge",
+          "text": "Hey!&#x1FAE1;",
+          "backgroundColor": "#00ABFF",
+          "position": "bottom-right",
+          "dismissible": true
+        }
+      };
+    `;
+    document.body.appendChild(videoAskScript);
+
+    const embedScript = document.createElement('script');
+    embedScript.src = 'https://www.videoask.com/embed/embed.js';
+    document.body.appendChild(embedScript);
+
+    return () => {
+      document.body.removeChild(videoAskScript);
+      document.body.removeChild(embedScript);
+    };
+  }, []);
 
   return (
     <MuiThemeProvider theme={MuiTheme}>
