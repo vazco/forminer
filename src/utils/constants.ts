14cd05e56e28430e8288fe327d25e6f96af440a2
Piotr Pośpiech <ptkpospiech@gmail.com>
2023-09-20 15:32:45 +0200

    fixed stripe links

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const STRIPE_PAYMENT_LINKS = {
-  TRIAL:
-    'https://checkout.stripe.com/c/pay/cs_test_a1BI053Xp2JOveJIRSTdZECO5tkvHJXtsrzrbVngxCfn4QSHHl8sBOUlNO#fidkdWxOYHwnPyd1blpxYHZxWjA0SEhxbVxNQmcybzJvVHJNdjNWM2tEfFRKcmpKMTdPbUlvNzxXYF1%2FTEhLb0tmZDdcazJdS1dKMlxVdHBLZlx2dUxAMG1UYlJnajF2SXZEVGFKSEFVMnI3NTU1T0t2T0RcPScpJ3VpbGtuQH11anZgYUxhJz8ncWB2cVoxYnIwaFNmVGIzZldjM3I9cn0nKSd1d2BpamRhQ2prcSc%2FJ0xrcWB3J3gl',
+  TRIAL: 'https://buy.stripe.com/test_4gw5mVcQg6cRf6w8wx',
+  LIFELONG: 'https://buy.stripe.com/test_6oEg1zeYodFj7E4eUW',
 };
