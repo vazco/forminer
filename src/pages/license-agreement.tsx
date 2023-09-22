ed90fbb6fed9d1b927a534194b030a0d69f6fa10
Piotr Pośpiech <ptkpospiech@gmail.com>
2023-09-22 13:37:52 +0200

    refactor

--- src/pages/license-agreement.tsx
+++ src/pages/license-agreement.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LicenseAgreement = () => {
   return (
     <iframe
-      src="http://localhost:3000/pdf/license-agreement.pdf"
+      src="https://forminer.com/pdf/license-agreement.pdf"
       title="License Agreement"
     />
   );
