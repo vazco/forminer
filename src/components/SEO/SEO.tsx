648861fd943782da4118874d086e5171b7f45d99
Piotr Pośpiech <ptkpospiech@gmail.com>
2023-10-25 12:48:14 +0200

    added clarity meta tag

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -6,6 +6,10 @@ import seoData from './seo-data.json';
 export const SEO = () => (
   <Head>
     <title>{seoData.title}</title>
+    <meta
+      name="clarity-site-verification"
+      content="482a972d-1709-4257-8039-d59297fcba91"
+    />
     <meta name="description" content={seoData.description} />
     <meta property="robots" content={seoData.robots} />
   </Head>
