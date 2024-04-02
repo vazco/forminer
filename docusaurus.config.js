67e3d24939d814cee45149da5ed6f5159a318412
Piotr Pośpiech <ptkpospiech@gmail.com>
2024-04-02 11:38:35 +0200

    fixed path

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
           versions: {
             current: {
               label: '2.x',
-              path: '2.x',
+              path: '',
             },
           },
           routeBasePath: '/docs',
