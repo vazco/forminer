3f81c3866a5cc09fa80f8e45f7da4ca6ed336481
Piotr Pośpiech <37746259+piotrpospiech@users.noreply.github.com>
2024-10-23 12:17:41 +0200

    updated pricing section (#71)

--- emails/README.md
+++
@@ -1,3 +0,0 @@
-This is email template created with Beefree Trial version. It is used in Zapier (Stripe + Gmail integration) to send Forminer source code to customer after completing checkout.
-
-Replace download link before updating it in Zapier.
--- src/sections/Pricing/Pricing.tsx
+++ src/sections/Pricing/Pricing.tsx
@@ -1,133 +1,55 @@
+import clsx from 'clsx';
 import React from 'react';
 
-// eslint-disable-next-line no-unused-vars -- false positive error
-import { PricingCard, PricingCardData } from '../../components/PricingCard';
 import { SectionLayout } from '../../components/SectionLayout';
 import CheckmarkIcon from '../../images/svg/checkmark.svg';
-import DiamondIcon from '../../images/svg/diamond.svg';
-import DollarIcon from '../../images/svg/dollar.svg';
-import GiftIcon from '../../images/svg/gift.svg';
-import StarIcon from '../../images/svg/star.svg';
-import { STRIPE_PAYMENT_LINKS } from '../../utils/constants';
-
-const pricingCards: PricingCardData[] = [
-  {
-    color: '#818A96',
-    title: 'Trial',
-    price: 'Free',
-    benefits: [
-      {
-        icon: <CheckmarkIcon />,
-        text: 'Use it in PoC (Proof of Concept) to see how it fits your project.',
-      },
-      {
-        icon: <CheckmarkIcon />,
-        text: 'Once ready to use it in your operations, purchase and enjoy it on production.',
-      },
-    ],
-    buttonText: 'Try it out',
-    buttonLink: '/trial',
-    btnClassName: 'btn-trial',
-  },
-  {
-    color: '#00ABFF',
-    title: 'Monthly',
-    price: '$149',
-    denominator: 'month',
-    benefits: [
-      {
-        icon: <StarIcon />,
-        text: 'All features included.',
-        isButton: true,
-        className: 'btn-all-features-monthly',
-      },
-    ],
-    buttonText: 'Buy now',
-    buttonLink: STRIPE_PAYMENT_LINKS.MONTHLY,
-    isMostPopular: true,
-    btnClassName: 'btn-buy-monthly',
-  },
-  {
-    color: '#0092DB',
-    title: 'Yearly',
-    price: '$1490',
-    denominator: 'year',
-    benefits: [
-      {
-        icon: <StarIcon />,
-        text: 'All features included.',
-        isButton: true,
-        className: 'btn-all-features-yearly',
-      },
-      {
-        icon: <GiftIcon />,
-        text: 'Two extra free months.',
-        additionalStyles: {
-          padding: 7,
-        },
-      },
-    ],
-    buttonText: 'Buy now',
-    buttonLink: STRIPE_PAYMENT_LINKS.YEARLY,
-    btnClassName: 'btn-buy-yearly',
-  },
-  {
-    color: '#0070A8',
-    title: 'Lifelong',
-    price: '$4900',
-    denominator: '∞',
-    benefits: [
-      {
-        icon: <StarIcon />,
-        text: 'All features included.',
-        isButton: true,
-        className: 'btn-all-features-lifelong',
-      },
-      {
-        icon: <DollarIcon />,
-        text: 'Pay once and enjoy it forever.',
-        additionalStyles: {
-          padding: 7,
-        },
-      },
-    ],
-    buttonText: 'Buy now',
-    buttonLink: STRIPE_PAYMENT_LINKS.LIFELONG,
-    btnClassName: 'btn-buy-lifelong',
-  },
-  {
-    color: '#C4BE14',
-    title: 'Custom',
-    benefits: [
-      {
-        icon: <DiamondIcon />,
-        text: 'Multi-project licenses',
-      },
-      {
-        icon: <DiamondIcon />,
-        text: 'Custom extensions',
-      },
-      {
-        icon: <DiamondIcon />,
-        text: 'Training sessions & consultancy',
-      },
-    ],
-    buttonText: 'Contact us',
-    buttonLink: 'https://www.vazco.eu/contact-us',
-    btnClassName: 'btn-buy-custom',
-  },
-];
 
 export const Pricing = () => {
   return (
     <SectionLayout heading="Pricing" id="pricing" centerHeading>
       <div className="pricing__description">
-        Interested in using Forminer? Find the plan that suits you best.
+        Are you considering leveraging Forminer for your projects?
       </div>
-      <div className="pricing__cards">
-        {pricingCards.map(pricingCard => (
-          <PricingCard key={pricingCard.title} {...pricingCard} />
-        ))}
+      <div className="pricing__card">
+        <div className="pricing__information">
+          <div className="pricing__title">CUSTOM</div>
+          <div className="pricing__wrapper">
+            <div className="pricing__subtitle">
+              Personalized solution designed to meet your needs
+            </div>
+            <p className="pricing__more-info">
+              Utilize our ready-made solution to fulfill your project
+              requirements, saving both time and budget instead of developing
+              something from scratch.
+            </p>
+            <a
+              className={clsx('button', 'pricing__btn', 'button__solid')}
+              href="https://www.vazco.eu/contact-us"
+              target="_blank"
+            >
+              Contact us
+            </a>
+          </div>
+        </div>
+        <div className="pricing__details">
+          <span className="pricing__features_title">INCLUDES:</span>
+          <ul>
+            <li>
+              <CheckmarkIcon width={20} height={20} />
+              Multi-project licenses
+            </li>
+            <hr />
+            <li>
+              <CheckmarkIcon width={20} height={20} />
+              Custom extensions
+            </li>
+            <hr />
+            <li>
+              <CheckmarkIcon width={20} height={20} />
+              Training sessions & consultancy
+            </li>
+          </ul>
+        </div>
       </div>
     </SectionLayout>
   );
