694a10baa0931cff765f6bf37f102204c542f2af
piotrpospiech <piotrpospiech@users.noreply.github.com>
2024-10-23 10:18:47 +0000

    deploy: 3f81c3866a5cc09fa80f8e45f7da4ca6ed336481

--- assets/js/422532b0.bc95c2c8.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkforminer=self.webpackChunkforminer||[]).push([[401],{9504:(e,r,t)=>{t.r(r),t.d(r,{default:()=>s});var n=t(7294);const s=()=>n.createElement("iframe",{src:"https://forminer.com/pdf/license-agreement.pdf",title:"License Agreement"})}}]);
